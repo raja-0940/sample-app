f7bfa2d7b798c598d4d985b4b6b3c472c227ba86
rajakumar battula <rkb0940@gmail.com>
2023-12-12 04:06:39 -0800

    updated

--- run-app.sh
+++ run-app.sh
@@ -8,5 +8,5 @@ docker ps `docker ps -a -q`
 
 docker build -t sample-app:latest-${RANDOM_NUM} .
 
-docker run --name sample-app -p 8080:8080 sample-app:latest-${RANDOM_NUM}
+docker run --name sample-app-${RANDOM_NUM} -p 8080:8080 sample-app:latest-${RANDOM_NUM}
 
