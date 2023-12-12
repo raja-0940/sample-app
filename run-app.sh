4b75f3cb8f1683183e82a4e7646c5b9d321738d4
rajakumar battula <rkb0940@gmail.com>
2023-12-12 03:25:25 -0800

    version updated

--- run-app.sh
+++ run-app.sh
@@ -2,7 +2,7 @@
 
 APP_DIR="/root/dev_ops/devops_maven/hello-world"
 
-docker build -t sample-app:latest $APP_DIR
+docker build -t sample-app:latest-v3 $APP_DIR
 
-docker run --name sample-app -p 8080:8080 sample-app:latest
+docker run --name sample-app -p 8080:8080 sample-app:latest-v3
 
