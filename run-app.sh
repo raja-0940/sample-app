22675a5b7022d739f2d44ddedd386497f99f373f
rajakumar battula <rkb0940@gmail.com>
2023-12-12 03:49:37 -0800

    updated run-app.sh

--- run-app.sh
+++ run-app.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
+RANDOM_NUM="${RANDOM}"
+
 docker rmi `docker images -a -q`
 
 docker ps `docker ps -a -q`
 
-docker build -t sample-app:latest-${RANDOM} .
+docker build -t sample-app:latest-${RANDOM_NUM} .
 
-docker run --name sample-app -p 8080:8080 sample-app:latest-${RANDOM}
+docker run --name sample-app -p 8080:8080 sample-app:latest-${RANDOM_NUM}
 
