197a3881c941c40a3f932718b21cdef93c4d516f
rajakumar battula <rkb0940@gmail.com>
2023-12-12 03:41:17 -0800

    updated run-app.sh

--- run-app.sh
+++ run-app.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+docker rmi `docker images -a -q`
+
+docker ps `docker ps -a -q`
+
 docker build -t sample-app:latest-v3 .
 
 docker run --name sample-app -p 8080:8080 sample-app:latest-v3
